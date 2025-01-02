bc922ab3c33d802c09162812346ccb8af9cf0ac0
George Tziavas <g.tziavas@ac.upatras.gr>
2025-01-02 18:47:45 +0000

    Resolve "TMF628: Add ExecutionState attribute to MeasurementCollectionJobMVO through inheritance from ManagementJobMVO"

--- src/main/java/org/etsi/osl/tmf/pm628/model/ResourceRefOrValue.java
+++ src/main/java/org/etsi/osl/tmf/pm628/model/ResourceRefOrValue.java
@@ -22,4 +22,5 @@ import javax.annotation.Generated;
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2024-05-15T07:30:16.936523289Z[Etc/UTC]", comments = "Generator version: 7.6.0-SNAPSHOT")
 public interface ResourceRefOrValue {
     public String getType();
+    public void setType(String type);
 }
